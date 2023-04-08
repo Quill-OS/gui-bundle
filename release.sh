7a35fec124d548ba7b6369519855be2a85deed6e
Nicolas Mailloux <nicolecrivain@gmail.com>
2023-04-08 00:01:41 -0400

    Final commit before merge

--- release.sh
+++ release.sh
@@ -5,13 +5,16 @@ MKSQUASHFS_ARGS=('-b' '1048576' '-comp' 'xz' '-Xdict-size' '100%' '-always-use-f
 [ -z "${GITDIR}" ] && echo "Please provide the GITDIR environment variable." && exit 1
 [ -z "${1}" ] && echo "Please provide the 'signature key' argument." && exit 1
 [ -z "${2}" ] && echo "Please provide the 'version' argument." && exit 1
+[ -z "${3}" ] && echo "Please provide the 'binaries build folders location' argument." && exit 1
 
 cd "${GITDIR}"
 
 # Copying compiled binaries
-cp "build/build_inkbox/inkbox" "content/inkbox/inkbox-bin"
-cp "build/build_oobe-inkbox/oobe-inkbox" "content/inkbox/oobe-inkbox-bin"
-cp "build/build_lockscreen/lockscreen" "content/inkbox/lockscreen-bin"
+pushd "content/inkbox"
+cp "${3}/build_inkbox/inkbox" "./inkbox-bin"
+cp "${3}/build_oobe-inkbox/oobe-inkbox" "./oobe-inkbox-bin"
+cp "${3}/build_lockscreen/lockscreen" "./lockscreen-bin"
+popd
 
 # Squashing packages
 rm -rf "out/"
@@ -23,7 +26,7 @@ mksquashfs "${GITDIR}/content/inkbox" "./inkbox.isa" "${MKSQUASHFS_ARGS[@]}"
 mksquashfs "${GITDIR}/content/qt" "./qt.isa" "${MKSQUASHFS_ARGS[@]}"
 for f in *.isa; do
 	if [ "${f}" != "*" ]; then
-		openssl dgst -sha256 -sign "${GITDIR}/${1}" -out "${f}.dgst" "${f}"
+		openssl dgst -sha256 -sign "${1}" -out "${f}.dgst" "${f}"
 	fi
 done
 sync
