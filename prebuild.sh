752d44383ce5cfc4ac35ff850b2346f7c1b1716c
Nicolas Mailloux <63172576+tux-linux@users.noreply.github.com>
2023-07-25 17:43:17 -0400

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-export PATH=$PATH:/home/build/inkbox/compiled-binaries/arm-kobo-linux-gnueabihf/bin/
+[ -z "${THREADS}" ] && echo "Warning: THREADS environment variable wasn't set. Running with only one thread." && THREADS=1
+
+export PATH="${PATH}:/home/build/inkbox/compiled-binaries/arm-kobo-linux-gnueabihf/bin/"
 
 rm -rf build
 mkdir build
-cd build
+pushd build/
 
 git clone https://github.com/Kobo-InkBox/inkbox.git build_inkbox
 git clone https://github.com/Kobo-InkBox/lockscreen.git build_lockscreen
-git clone https://github.com/Kobo-InkBox/oobe-inkbox.git build_oobe-inkbox\
+git clone https://github.com/Kobo-InkBox/oobe-inkbox.git build_oobe-inkbox
 
-cd build_inkbox/
+pushd build_inkbox/
 git submodule update --init --recursive
 /home/build/inkbox/compiled-binaries/qt-bin/qt-linux-5.15.2-kobo/bin/qmake
-make -j 16
-cd ..
+make -j${THREADS}
+popd
 
-cd build_lockscreen/
+pushd build_lockscreen/
 /home/build/inkbox/compiled-binaries/qt-bin/qt-linux-5.15.2-kobo/bin/qmake
-make -j 16
-cd ..
+make -j${THREADS}
+popd
 
-cd build_oobe-inkbox/
+pushd build_oobe-inkbox/
 /home/build/inkbox/compiled-binaries/qt-bin/qt-linux-5.15.2-kobo/bin/qmake
-make -j 16
-cd ..
+make -j${THREADS}
+popd
 
-cd ..
+popd
