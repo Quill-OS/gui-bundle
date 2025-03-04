ca3c1db55799f6a47606c92ef1612ef376a0970e
Nicolas Mailloux <nicolecrivain@gmail.com>
2025-03-04 17:29:52 -0500

    Make GZip compression the default

--- release.sh
+++ release.sh
@@ -5,10 +5,10 @@ KOREADER_VERSION="2024.11"
 if [ "$NO_COMPRESSION" == "true" ]; then
 	echo "Not using SquashFS compression"
 	MKSQUASHFS_ARGS=('-b' '1048576' '-always-use-fragments' '-noI' '-noD' '-noF' '-noX')
-elif [ "${GZIP_COMPRESSION}" == "true" ]; then
-	MKSQUASHFS_ARGS=('-b' '1048576' '-always-use-fragments')
-else
+elif [ "${XZ_COMPRESSION}" == "true" ]; then
 	MKSQUASHFS_ARGS=('-b' '1048576' '-comp' 'xz' '-Xdict-size' '100%' '-always-use-fragments')
+else
+	MKSQUASHFS_ARGS=('-b' '1048576' '-always-use-fragments')
 fi
 
 [ -z "${GITDIR}" ] && echo "Please provide the GITDIR environment variable." && exit 1
